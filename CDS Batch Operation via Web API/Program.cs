bbb5f8421c5a4f85b160e70d75430df172fb8056
Jens Schrøder <systemadmin@jschroder.onmicrosoft.com>
2020-03-12 21:26:51 +0100

    Add project files.

---
+++ CDS Batch Operation via Web API/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDS_Batch_Operation_via_Web_API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
